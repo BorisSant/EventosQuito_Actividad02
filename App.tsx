81bba28055dc07de5b088d46c52844f8d1f33294
BorisSant <boris.santiago.t@gmail.com>
2024-01-05 23:02:23 -0500

    Inicio de actividad 02

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
